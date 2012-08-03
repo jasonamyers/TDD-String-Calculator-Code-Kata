c50af70f3d861fd57da0a01f20a53b007562e11d
Jason Myers <jason@jasonamyers.com>
2012-08-03 08:29:07 -0500

    Post Video

--- stringcalc.php
+++ stringcalc.php
@@ -2,6 +2,20 @@
 
 // String Calculator Code Kata 
 
+class StringCalculator {
 
+	public function add($string) {
+		if (strlen($string) > 0) {
+			$sum = 0;
+			$numbers = preg_split("/[\\n|,]+/", $string);
+			foreach($numbers as $number) {
+				$sum += $number;
+			}
+			return $sum;
+		} else {
+			return 0;
+		}
+	}
 }
+
 ?>
--- stringcalctests.php
+++ stringcalctests.php
@@ -2,6 +2,68 @@
 
 //String Calculator Code Kata Tests
 
+require_once 'stringcalc.php';
+require_once 'PHPUnit/Autoload.php';
 
+class StringCalculatorTests extends PHPUnit_Framework_TestCase {
+
+	protected $stringcalc;
+
+	protected function setUp() {
+		$this->stringcalc = new StringCalculator();
+	}
+
+	protected function tearDown() {
+		unset($this->stringcalc);
+	}
+
+	function testAddEmptyString() {
+		$expected = 0;
+		$actual = $this->stringcalc->add("");
+		$this->assertEquals($expected, $actual);
+	}
+
+	function testAddOneNumberStringOfOne() {
+		$expected = 1;
+		$actual = $this->stringcalc->add("1");
+		$this->assertEquals($expected, $actual);
+	}
+
+	function testAddOneNumberStringOfTwo() {
+		$expected = 2;
+		$actual = $this->stringcalc->add("2");
+		$this->assertEquals($expected, $actual);
+	}
+
+	function testAddTwoNumberStringofOneTwo() {
+		$expected = 3;
+		$actual = $this->stringcalc->add("1,2");
+		$this->assertEquals($expected, $actual);
+	}
+
+	function testAddTwoNumberStringofTwoThree() {
+		$expected = 5;
+		$actual = $this->stringcalc->add("2,3");
+		$this->assertEquals($expected, $actual);
+	}
+
+	function testAddThreeNumberStringofOneTwoThree() {
+		$expected = 6;
+		$actual = $this->stringcalc->add("1,2,3");
+		$this->assertEquals($expected, $actual);
+	}
+
+	function testAddFourNumberStringofTenNineEightSeven() {
+		$expected = 34;
+		$actual = $this->stringcalc->add("10,9,8,7");
+		$this->assertEquals($expected, $actual);
+	}
+
+	function testAddThreeNumberStringWithNewLineDelimiter() {
+		$expected = 6;
+		$actual = $this->stringcalc->add("1\n2,3");
+		$this->assertEquals($expected, $actual);
+	}
 }
+
 ?>
